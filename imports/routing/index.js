8aca32faaae689c7e5a521f8dc8b1346303d98ec
katebe <robam.katebe@gmail.com>
2018-05-22 15:56:42 +0200

    Added route for RoutesComponent to the Navbar

--- imports/routing/index.js
+++ imports/routing/index.js
@@ -3,15 +3,12 @@ import HomePage from '/imports/ui/HomePage.jsx';
 import ReservationForm from '/imports/ui/ReservationForm';
 import CreateAccount from '/imports/ui/CreateAccount.jsx';
 import Login from '/imports/ui/LogIn.jsx'
-import RoutesForm from '/imports/ui/RoutesComponent.jsx';
+import RoutesComponent from '/imports/ui/RoutesComponent.jsx';
 
 
 
-route ('/Createaccount', CreateAccount);
+route ('/CreateAccount', CreateAccount);
 route ('/Login', Login);
 route('/', HomePage);
 route('/ReservationForm', ReservationForm)
-route('/RoutesForm',RoutesForm);
-// route('/CreateAccount',CreateAccount);
-// route('/ReserveForm',ReserveForm);
-// route('/LogIn',LogIn);
+route('/Routes',RoutesComponent);
