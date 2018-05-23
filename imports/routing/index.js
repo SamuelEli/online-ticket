da680b71ef6c61984b415f4c6ed3374ebda543ae
kapansa <samuelkapansa@gmail.com>
2018-05-23 12:43:16 +0200

    added another Routes table

--- imports/routing/index.js
+++ imports/routing/index.js
@@ -3,12 +3,13 @@ import HomePage from '/imports/ui/HomePage.jsx';
 import ReservationForm from '/imports/ui/ReservationForm';
 import CreateAccount from '/imports/ui/CreateAccount.jsx';
 import Login from '/imports/ui/LogIn.jsx'
-import RoutesComponent from '/imports/ui/RoutesComponent.jsx';
-
+import Routes from '/imports/ui/RoutesComponent.jsx';
+import RoutesForm2 from '/imports/ui/RoutesComponent2.jsx';
 
 
 route ('/CreateAccount', CreateAccount);
 route ('/Login', Login);
 route('/', HomePage);
 route('/ReservationForm', ReservationForm)
-route('/Routes',RoutesComponent);
+route('/Routes',Routes);
+route('/RoutesForm2',RoutesForm2);
--- imports/ui/RoutesComponent2.jsx
+++ imports/ui/RoutesComponent2.jsx
@@ -5,7 +5,7 @@ import Footer from '/imports/ui/Footer.jsx';
 import { Row, Input, Table } from 'react-materialize';
 
 
-export default  class RoutesForm extends Component{
+export default  class RoutesComponent2 extends Component{
   render(){
     return(
       <div>
@@ -13,7 +13,8 @@ export default  class RoutesForm extends Component{
           <Navbar/>
         </div>
         <br/>
-          <h1>local fares table</h1>
+        <h1>international fares table</h1>
+
         <div>
       <Table className="sarah">
         <thead>
@@ -28,47 +29,47 @@ export default  class RoutesForm extends Component{
         <tbody>
           <tr>
             <td>LUSAKA</td>
-            <td>SOLWEZI</td>
+            <td>MALAWI</td>
             <td>04:30hours</td>
             <td>K85</td>
           </tr>
 
           <tr>
             <td>LIVINGSTONE</td>
-            <td>KITWE</td>
+            <td>ZIMBABWE</td>
             <td>04:00hours</td>
             <td>K70</td>
           </tr>
           <tr>
-            <td>KITWE</td>
+            <td>DRC</td>
             <td>NDOLA</td>
             <td>07.00hours</td>
             <td>K90</td>
           </tr>
           <tr>
             <td>LIVINGSTONE</td>
-            <td>CHAIPATA</td>
+            <td>NAMIBIA</td>
             <td>16.00hours</td>
             <td>K70</td>
           </tr>
 
           <tr>
+            <td>ANGOLAs</td>
             <td>SOLWEZI</td>
-            <td>LUANGWA</td>
             <td>8.00hours</td>
             <td>k150</td>
           </tr>
 
           <tr>
-            <td>CHINGOLA</td>
+            <td>TANZANIA</td>
             <td>MANSA</td>
             <td>09.00hours</td>
             <td>k100</td>
           </tr>
 
           <tr>
-            <td>MPIKA</td>
             <td>CHOMA</td>
+            <td>ZIMBABWE</td>
             <td>15:00hours</td>
             <td>k50</td>
 
@@ -76,35 +77,35 @@ export default  class RoutesForm extends Component{
 
           <tr>
             <td>SIAVONGA</td>
-            <td>MONGO</td>
+            <td>BOTSWANA</td>
             <td>12:00hours</td>
             <td>k120</td>
           </tr>
 
 
           <tr>
-            <td>KAFUE</td>
             <td>CHILLIABOMBWE</td>
+            <td>DRC</td>
             <td>10:00hours</td>
             <td>k60</td>
           </tr>
 
           <tr>
-            <td>MAZABUKA</td>
             <td>KASAMA</td>
+            <td>NAMIBAIA</td>
             <td>15:00hours</td>
             <td>K150</td>
           </tr>
 
           <tr>
-            <td>KABWE</td>
-            <td>MONZE</td>
+            <td>LUSAKA</td>
+            <td>TANZANIA</td>
             <td>15:00hours</td>
             <td>K150</td>
           </tr>
           <tr>
             <td>kITWE</td>
-            <td>NDOLA</td>
+            <td>SOUTHAFRICA</td>
             <td>07.00hours</td>
             <td>K90</td>
           </tr>
